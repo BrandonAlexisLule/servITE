c6f2da8dc2bb021968a06de9a4603b4d530ec332
BrandonAlexisLule <al19760600@ite.edu.mx>
2025-05-21 23:02:15 -0700

    Probando subida

--- lib/supabase.tsx
+++ lib/supabase.tsx
@@ -2,8 +2,8 @@ import { createClient } from '@supabase/supabase-js';
 
 
 
-const url = 'https://jnnsprdbewsjthsolvar.supabase.co'
-const key = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpubnNwcmRiZXdzanRoc29sdmFyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc0NTcwMTEsImV4cCI6MjA2MzAzMzAxMX0.fgpG2e8DC-a7uogEULPfC6lHlSY226jUdRuf0-k5qds'
+const url = ''
+const key = ''
 
 export const supabase = createClient(url, key,{
     auth:{
