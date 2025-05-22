909df996111018cfd1dc8972b655581044fc5fc8
BrandonAlexisLule <al19760600@ite.edu.mx>
2025-05-21 23:03:28 -0700

    Probando subida

--- lib/supabase.tsx
+++ lib/supabase.tsx
@@ -1,6 +1,9 @@
 import { createClient } from '@supabase/supabase-js';
 
 
+//Brandon
+//const url = 'https://jnnsprdbewsjthsolvar.supabase.co'
+//const key = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpubnNwcmRiZXdzanRoc29sdmFyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc0NTcwMTEsImV4cCI6MjA2MzAzMzAxMX0.fgpG2e8DC-a7uogEULPfC6lHlSY226jUdRuf0-k5qds'
 
 const url = ''
 const key = ''
